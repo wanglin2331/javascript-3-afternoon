f18ff629982968b2c224e6e62a67f69d95413d7a
Lin Wang <lin.wang@healthcatalyst.com>
2018-01-17 16:33:26 -0700

    all done

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(num){return num%2==0})
 
 
 
@@ -44,7 +44,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices  = prices.map(function(num){return num*1.07});
 
 
 
@@ -63,7 +63,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  = populations.reduce(function(accum,num){return accum+num});
 
 
 
@@ -89,7 +89,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(v,CP){
+  return v.CP>200
+})
 
 
 
@@ -106,7 +108,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a high order method to get sum of all the order totals
 */
 
-let ordersTotal //Code Here
+let ordersTotal = orders.map(function(v,price,tax){
+    return v.price*(1+v.tax)
+})
 
 
 
@@ -125,7 +129,13 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 /*
   Use a high order method to create to get the sum of bobsTotal.
 */
+let bobsObj = purchases.filter(function(i){
+  if(i.owner==="Bob") {return i.price}
+              });
 
-let bobsTotal //Code Here
+let bobsArray=bobsObj.map(function(obj){
+  return obj['price']});
 
+let bobsTotal=bobsArray.reduce(function(accum,num){
+  return accum + num});
 
